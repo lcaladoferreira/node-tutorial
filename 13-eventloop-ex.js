f0575bb259f09ce163e6eed262b41b4ef5909b29
lcaladoferreira <83848600+lcaladoferreira@users.noreply.github.com>
2022-11-19 13:32:38 -0300

    eventloop ex

---
+++ 13-eventloop-ex.js
@@ -0,0 +1,34 @@
+// const _= require("lodash");
+// // const inverval = require("./2-setInterval");
+
+// // const exemplo = inverval.setInterval(()=>{
+// //     console.log('teste do app')
+// // }, 2000)
+
+// const items = [1, [2, [3, [4]]]]
+// const newItems = _.flattenDeep(items);
+// console.log(newItems)
+// console.log("hello people")
+
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     console.log("request event")
+//     res.end("hey people")
+// })
+// server.listen(3001, ()=>{
+//     console.log("listening on http://localhost:3001")
+// })
+
+// console.log('primeiro')
+// setTimeout(()=>{
+//     console.log('terceiro')
+// }, 2000)
+// console.log('segundo');
+
+
+// setInterval(()=>{
+//     console.log("Ta, ta, ta, ta, ta")
+// }, 5000)
+// console.log("Dona Florinda")
+
+// module.exports ={setInterval}
--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const _= require("lodash");
-const inverval = require("./2-setInterval");
+// const _= require("lodash");
+// // const inverval = require("./2-setInterval");
 
-const exemplo = inverval.setInterval(()=>{
-    console.log('teste do app')
-}, 2000)
+// // const exemplo = inverval.setInterval(()=>{
+// //     console.log('teste do app')
+// // }, 2000)
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems)
-console.log("hello people")
+// const items = [1, [2, [3, [4]]]]
+// const newItems = _.flattenDeep(items);
+// console.log(newItems)
+// console.log("hello people")
 
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log("request event")
+    res.end("hey people")
+})
+server.listen(3001, ()=>{
+    console.log("listening on http://localhost:3001")
+})
